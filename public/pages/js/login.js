7aad71ad456f2ba8e88f0ff3bd7d91475b7487a8
Nikon-knon <nkon@umass.edu>
2020-04-24 11:36:03 -0400

    updated full functionality in login and signup pages (pre-auth)

--- public/pages/js/login.js
+++ public/pages/js/login.js
@@ -1,9 +1,23 @@
 
 var modal = document.getElementById("myModal");
 
+var recoverMod = document.getElementById("recoverModal");
+
 var btn = document.getElementById("myBtn");
+
+var recover = document.getElementById("recoverPsw");
+
+var span = document.getElementsByClassName("close")[0];
+
+recover.onclick = function() {
+  recoverMod.style.display = "block";
+}
 
 btn.onclick = function() {
   modal.style.display = "block";
+}
+
+span.onclick = function() {
+  recoverMod.style.display = "none";
 }
 
