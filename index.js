b0a465ada11b5719606e86b500f74fc62e8ac1cd
Sarah Young <seaynne@gmail.com>
2020-05-07 13:46:00 -0500

    increase payload size for imgs

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 //the port the server should be listening on 
 var PORT = process.env.PORT || 5657;
+// increase payload
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // static home page 
 app.use(express.static(path.join(__dirname, 'public/pages')));
 //start listening on the port specified
 app.listen(PORT, function () { return console.log("Server started on port " + PORT); });
+//Handlebar middleware 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //route to the different pages
